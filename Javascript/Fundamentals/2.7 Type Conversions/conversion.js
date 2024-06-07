af595dddabd6957c281073931eeed4b530396759
LongTangGithub <tanglh@g.cofc.edu>
2024-06-07 14:51:53 -0400

    conversion done

--- Javascript/Fundamentals/2.7 Type Conversions/conversion.js
+++ Javascript/Fundamentals/2.7 Type Conversions/conversion.js
@@ -1,5 +1,5 @@
 /**
- * ! String Conversion
+ * ! String Conversion --> Occurs when we output something
  * ? String Conversion occurs when we want to convert a value to a string
  */
 
@@ -9,9 +9,11 @@ value = String(value);
 console.log(typeof value);          // string
 
 /**
- * ! Numeric Conversion
+ * ! Numeric Conversion --> Occurs in math operations
  * ? Numeric conversion in mathematical functions and expressions happens 
  * ? automically 
+ * 
+ * ? If the string is not a valid number, the result of the conversion is "NaN"
  * 
  */
 
@@ -21,3 +23,24 @@ console.log("6" / 2);         // 3, same thing applies
 let numStr = "123";
 numStr = Number(numStr);
 console.log(numStr);        // 123
+
+let age = "Eighteen";
+//console.log(age);         // NaN, conversion failed
+console.log(Number("123"))
+console.log(Number("12)3"))
+
+
+/**
+ * ! Boolean Conversion
+ * ? Values that are "empty", like 0, emoty string, null, undefined, and Nan becomes false
+ * ? Other values become true
+ */
+
+console.log(Boolean(1));            // true
+console.log(Boolean(0));           // false
+
+console.log(Boolean("hello"));    // true
+console.log(Boolean(""));        // false
+
+console.log("0");               // true
+console.log(" ");              // true  ---> due to spaces
